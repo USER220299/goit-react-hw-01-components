09505cbd999a13f478f2ca6604b84d220b592e25
USER220299 <127625631+USER220299@users.noreply.github.com>
2023-09-14 22:46:53 +0300

    add

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,19 @@
+
+export const Statistics = ({ stats}) => {
+    return (
+    <section class="statistics">
+  <h2 class="title">Upload stats</h2>
+
+  <ul class="stat-list">
+    {stats.map(item=>
+      <li class="item" key={item.id}>
+      <span class="label">{item.label}</span>
+      <span class="percentage">{item.percentage}</span>
+    </li>
+
+    )}
+    
+  </ul>
+</section>
+    )
+}
